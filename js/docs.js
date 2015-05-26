10149fc21aa17bc38bebc83e3eb7c070d9ba7228
Siebert Lubbe <siebert.lubbe@hubonline.com.au>
2015-05-26 13:27:14 +1000

    Using the images from a hidden template element as it does not mess up the dimensions of the images

--- js/docs.js
+++ js/docs.js
@@ -126,8 +126,10 @@ $(function() {
   createPoster = function() {
     var dataURL = $image.cropper("getDataURL", "image/jpeg", 1);
     var posterTemplateDataURL = getImageDataURL($(".poster-template img")[0])
+    var posterHeaderDataURL = getImageDataURL($(".poster-template img")[1])
     var doc = new jsPDF('portrait', 'mm', 'a3');
     doc.addImage(posterTemplateDataURL, 'JPEG', 0, 0, 297, 420);
+    doc.addImage(posterHeaderDataURL, 'JPEG', 0, 0, 297, 66);
     doc.addImage(dataURL, 'JPEG', 0, 132, 297, 170);
     // doc.text(100,35, $("#riderName")[0].value);
     doc.text(40, 368, $("#riderName")[0].value);
